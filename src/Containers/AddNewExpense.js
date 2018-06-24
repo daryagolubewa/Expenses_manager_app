d763e251455ae1ab8f782016c600d76dea819a00
daryagolubewa <darya.golubewa@gmail.com>
2018-06-24 23:38:52 +0300

    AddNewExpense component created;

---
+++ src/Containers/AddNewExpense.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from "react-redux"
+import { addExpense } from "../Actions/Actions"
+import { FormControl, Button, FormGroup, Form, Row, Col, Panel } from 'react-bootstrap'
+
+const AddExpense = ({ dispatch }) => {
+    let title, userName, text
+    return(
+        <Row>
+            <Col md={4}>
+                <Panel bsStyle='primary'>
+                    <Panel.Body>
+                        <Form horizontal
+                              onSubmit={e => {
+                                  e.preventDefault()
+                                  if (!name.value.trim() || !sum.value.trim() || !date.value.trim()) {
+                                      return
+                                  }
+
+                                  dispatch(addExpense(name.value, sum.value, date.value))
+                                  name.value = '';
+                                  sum.value = '';
+                                  date.value = '';
+                              }}
+                        >
+
+                            <h3>Написать пост</h3>
+                            <FormGroup>
+                                <FormControl componentClass="textarea"  type='textarea' inputRef={node => userName = node} placeholder="Введите описане"/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <FormControl type='text' inputRef={node => title = node} placeholder="Введите сумму"/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <FormControl type='text' inputRef={node => text = node} placeholder="Введите дату"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Button type="submit">
+                                    Отправить
+                                </Button>
+                            </FormGroup>
+                        </Form>
+                    </Panel.Body>
+                </Panel>
+            </Col>
+        </Row>
+    )
+}
+
+export default connect()(AddExpense)
