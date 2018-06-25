cd6054ebf50614b6acfec6a9a72323284c0546bf
daryagolubewa <darya.golubewa@gmail.com>
2018-06-25 15:35:36 +0300

    combineReducers is added; store is added;

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import combineReducers from './Reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(combineReducers)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+
