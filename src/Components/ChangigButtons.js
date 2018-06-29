865405444a683e142c97e8f891f3b09d691688fc
daryagolubewa <darya.golubewa@gmail.com>
2018-06-29 18:18:50 +0300

    Link component is added;

--- src/Components/ChangigButtons.js
+++ src/Components/ChangigButtons.js
@@ -1,13 +1,14 @@
 import React from "react"
 import PropTypes from 'prop-types'
 import {Grid, Row, Col, Button} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const ChangingButtons = ({editButton, removeButton}) => (
     <Grid>
         <Row>
             <Col>
                 <div>
-                    <Button onClick={editButton}>Редактировать</Button>
+                    <Link to='/edit/:id'><Button>Редактировать</Button></Link>
                     <Button onClick={removeButton}>Удалить</Button>
                 </div>
             </Col>
