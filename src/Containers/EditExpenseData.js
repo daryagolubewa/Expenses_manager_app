160b94c07a275b95f2f218b3095aa4b1e5a5c594
daryagolubewa <darya.golubewa@gmail.com>
2018-07-02 00:36:55 +0300

    EditExpenseData updated;

--- src/Containers/EditExpenseData.js
+++ src/Containers/EditExpenseData.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux'
-import { editExpense } from '../Actions/Actions'
 import EditExpense from '../Containers/EditExpense'
 
-const mapStateToProps = state => ({
-    expenses: state.expenses.find(expense => {
-        return expense === expense.id
+const mapStateToProps = (state, match) => ({
+    ...state.expenses.find(expense => {
+        return match.params.id === expense.id
         }
     )
 })
