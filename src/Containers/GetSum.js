67d3c73f440d0c5060a97de6e4d64d41f8a059fc
daryagolubewa <darya.golubewa@gmail.com>
2018-07-02 14:11:34 +0300

    dateFilter added to GetSum;

--- src/Containers/GetSum.js
+++ src/Containers/GetSum.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Sum from '../Components/Sum'
+import Filter from '../Containers/Filter'
 
 const mapStateToProps = state => ({
-    totalSum: state.expenses.reduce((sum, current) => {
+    totalSum: Filter(state.expenses, state.dateFilter).reduce((sum, current) => {
         return sum + current.sum
 }, 0)
 })
@@ -12,3 +13,4 @@ const mapStateToProps = state => ({
 export default connect (
     mapStateToProps
 )(Sum)
+
